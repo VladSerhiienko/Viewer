40d6c03118c1dcc38950d6a4e0d595b9cdb68126
Vlad Serhiienko <vlad.serhiienko@gmail.com>
2018-06-03 23:20:03 +0300

    Removed dependency from filesystem in AssetManager, added tinydir library.

--- src/shared/platform/AssetManager.cpp
+++ src/shared/platform/AssetManager.cpp
@@ -3,19 +3,23 @@
 #include <FileReader.h>
 #include <AppState.h>
 
+#include <stdlib.h>
+#include <tinydir.h>
+
 #include <fstream>
 #include <iterator>
 #include <set>
 #include <regex>
 
-#if defined( __clang__ ) || defined( __GNUC__ )
-#include <experimental/filesystem>
-#else
-#include <filesystem>
-#endif
-namespace std {
-    namespace filesystem = std::experimental::filesystem::v1;
-}
+
+//#if defined( __clang__ ) || defined( __GNUC__ )
+//#include <experimental/filesystem>
+//#else
+//#include <filesystem>
+//#endif
+//namespace std {
+//    namespace filesystem = std::experimental::filesystem::v1;
+//}
 
 const std::string& apemodeos::AssetFile::GetName( ) const {
     return RelPath;
@@ -34,7 +38,17 @@ std::vector< uint8_t > apemodeos::AssetFile::AsBin( ) const {
 }
 
 inline std::string ToCanonicalAbsolutPath( std::string InRelativePath ) {
-    InRelativePath = std::filesystem::canonical( std::filesystem::absolute( InRelativePath ) ).string( );
+    // InRelativePath = std::filesystem::canonical( std::filesystem::absolute( InRelativePath ) ).string( );
+#if _WIN32
+    char canonicalAbsolutePath[ 1024 ] = {0};
+    if ( !_fullpath( canonicalAbsolutePath, InRelativePath.c_str( ), sizeof( canonicalAbsolutePath ) ) ) {
+        return "";
+    }
+
+    InRelativePath = canonicalAbsolutePath;
+#else
+    InRelativePath = realpath( InRelativePath.c_str( ), NULL );
+#endif
     std::replace( InRelativePath.begin( ), InRelativePath.end( ), '\\', '/' );
     return InRelativePath;
 }
@@ -42,6 +56,27 @@ inline std::string ToCanonicalAbsolutPath( std::string InRelativePath ) {
 const apemodeos::IAsset* apemodeos::AssetManager::GetAsset( const std::string& InAssetName ) const {
     auto assetIt = AssetFiles.find( InAssetName );
     return assetIt == AssetFiles.end( ) ? 0 : &assetIt->second;
+}
+
+template < typename TFileCallback >
+void ProcessFiles( TFileCallback callback, const tinydir_dir& dir, bool r ) {
+    auto appState = apemode::AppState::Get( );
+    appState->Logger->info( "AssetManager: Entering folder: {}", dir.path );
+
+    for ( size_t i = 0; i < dir.n_files; i++ ) {
+        tinydir_file file;
+        if ( tinydir_readfile_n( &dir, &file, i ) != -1 )
+            if ( file.is_reg ) {
+                appState->Logger->info( "AssetManager: Processing file: {}", file.path );
+                callback( file.path, file );
+            } else if ( r && file.is_dir && strcmp( file.name, "." ) != 0 && strcmp( file.name, ".." ) != 0 ) {
+                tinydir_dir subdir;
+                if ( tinydir_open_sorted( &subdir, file.path ) != -1 ) {
+                    ProcessFiles( callback, subdir, r );
+                    tinydir_close( &subdir );
+                }
+            }
+    }
 }
 
 void apemodeos::AssetManager::AddFilesFromDirectory( const std::string&                InStorageDirectory,
@@ -66,46 +101,48 @@ void apemodeos::AssetManager::AddFilesFromDirectory( const std::string&         
         }
     }
 
-    if ( std::filesystem::exists( storageDirectory ) ) {
+    tinydir_dir dir;
 
-        std::string storageDirectoryFull = ToCanonicalAbsolutPath( storageDirectory );
+    // if ( std::filesystem::exists( storageDirectory ) ) {
+    std::string storageDirectoryFull = ToCanonicalAbsolutPath( storageDirectory );
 
-        auto addFileFn = [&]( const std::filesystem::path& filePath ) {
-            if ( std::filesystem::is_regular_file( filePath ) ) {
+    // if ( !storageDirectoryFull.empty( ) && tinydir_open( &dir, storageDirectoryFull.c_str( ) ) != -1 ) {
+    if ( !storageDirectoryFull.empty( ) && tinydir_open_sorted( &dir, storageDirectoryFull.c_str( ) ) != -1 ) {
 
-                /* Check if file matches the patterns */
+        // auto addFileFn = [&]( const std::filesystem::path& filePath ) {
+        auto addFileFn = [&]( const std::string& filePath, tinydir_file file ) {
+            // tinydir_file file;
+            // if ( std::filesystem::is_regular_file( filePath ) ) {
+            // if ( tinydir_readfile( &dir, &file ) != -1 && file.is_reg ) {
+            /* Check if file matches the patterns */
 
-                bool matches = InFilePatterns.empty( );
-                for ( const auto& f : InFilePatterns ) {
-                    if ( std::regex_match( filePath.string( ), std::regex( f ) ) ) {
-                        matches = true;
-                        break;
-                    }
-                }
-
-                if ( matches ) {
-                    std::string fullPath = ToCanonicalAbsolutPath( filePath.string( ) );
-                    std::string relativePath = fullPath.substr( storageDirectoryFull.size() + 1 );
-
-                    auto assetIt = AssetFiles.find( relativePath );
-                    if ( assetIt == AssetFiles.end() ) {
-                        appState->Logger->info( "AssetManager: Added: {}", fullPath );
-                    } else {
-                        appState->Logger->warn( "AssetManager: Replaced: {} -> {}", assetIt->second.FullPath, fullPath );
-                    }
-
-                    AssetFiles[ relativePath ].RelPath = relativePath;
-                    AssetFiles[ relativePath ].FullPath = fullPath;
+            bool matches = InFilePatterns.empty( );
+            for ( const auto& f : InFilePatterns ) {
+                if ( std::regex_match( filePath, std::regex( f ) ) ) {
+                    matches = true;
+                    break;
                 }
             }
+
+            if ( matches ) {
+                const std::string fullPath = ToCanonicalAbsolutPath( filePath );
+                const std::string relativePath = fullPath.substr( storageDirectoryFull.size( ) + ( storageDirectoryFull.back( ) != '/' ) );
+
+                auto assetIt = AssetFiles.find( relativePath );
+                if ( assetIt == AssetFiles.end( ) ) {
+                    appState->Logger->info( "AssetManager: Added: {}", fullPath );
+                } else {
+                    appState->Logger->warn( "AssetManager: Replaced: {} -> {}", assetIt->second.FullPath, fullPath );
+                }
+
+                AssetFiles[ relativePath ].RelPath  = relativePath;
+                AssetFiles[ relativePath ].FullPath = fullPath;
+            }
+
+            //}
         };
 
-        if ( addSubDirectories ) {
-            for ( const auto& fileOrFolderPath : std::filesystem::recursive_directory_iterator( storageDirectoryFull ) )
-                addFileFn( fileOrFolderPath.path( ) );
-        } else {
-            for ( const auto& fileOrFolderPath : std::filesystem::directory_iterator( storageDirectoryFull ) )
-                addFileFn( fileOrFolderPath.path( ) );
-        }
+        ProcessFiles( addFileFn, dir, addSubDirectories );
+        tinydir_close( &dir );
     }
 }
